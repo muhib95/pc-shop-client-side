8193e96694a023eaf7cee182977689726d3e669a
Muhibbul hasan <muhibbulhasan6@gmail.com>
2023-01-11 23:31:20 +0600

    login phonenumber and email design

--- src/components/Page/Login/Login.js
+++ src/components/Page/Login/Login.js
@@ -1,7 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [phone, setPhone] = useState(true);
+  const [email, setEmail] = useState("");
+  const handlePhone = () => {
+    setPhone(true);
+    setEmail(false);
+  };
+  const handleEmail = () => {
+    setPhone(false);
+    setEmail(true);
+  };
+
+  return (
+    <div>
+      <button
+        onClick={() => handlePhone()}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+      >
+        Login with Phone
+      </button>
+
+      <button
+        onClick={() => handleEmail()}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+      >
+        Login with Email
+      </button>
+      {email && (
+        <div>
+          <form>
+            <input type="email" name="email" placeholder="Enter Email" />
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter Password"
+            />
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+              type="submit"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      )}
+      {phone && (
+        <div>
+          <form>
+            <input
+              type="number"
+              name="phoneNumber"
+              placeholder="Enter phone Number"
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter Password"
+            />
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+              type="submit"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Login;
